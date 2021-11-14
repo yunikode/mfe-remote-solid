f2767696a7894356a3a5ac54c6e5e9398408c6d0
Franz Lutzenberger <franz.lutzenberger@gmail.com>
2021-11-14 11:25:47 +0100

    update gitignore and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,62 +2,66 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
-  output: {
-    publicPath: "http://localhost:3000/",
-  },
+module.exports = (options) => {
+  return {
+    output: {
+      publicPath: options.WEBPACK_BUILD
+        ? "https://zee-german.github.io/mfe-remote-solid/"
+        : "http://localhost:3000/",
+    },
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
 
-  devServer: {
-    port: 3000,
-    historyApiFallback: true,
-  },
+    devServer: {
+      port: 3000,
+      historyApiFallback: true,
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
+          },
         },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
         },
-      },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "remote",
+        filename: "remoteEntry.js",
+        remotes: {},
+        exposes: {
+          "./Counter": "./src/Counter.jsx",
+          "./counterWrapper": "./src/counterWrapper.jsx",
+        },
+        shared: {
+          ...deps,
+          "solid-js": {
+            singleton: true,
+            requiredVersion: deps["solid-js"],
+          },
+        },
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
     ],
-  },
-
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "remote",
-      filename: "remoteEntry.js",
-      remotes: {},
-      exposes: {
-        "./Counter":"./src/Counter.jsx",
-        "./counterWrapper":"./src/counterWrapper.jsx",
-      },
-      shared: {
-        ...deps,
-        "solid-js": {
-          singleton: true,
-          requiredVersion: deps["solid-js"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+  };
 };
