977adfd5d78c40d99ecce8b73264d338f6f0e190
Franz Lutzenberger <franz.lutzenberger@gmail.com>
2021-11-17 10:39:58 +0100

    color change test

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,7 +4,7 @@ export default () => {
     const [count, setCount] = createSignal(0)
 
     return (
-        <div class='bg-blue-900 text-white p-5'>
+        <div class='bg-green-900 text-white p-5'>
             <div>Count = {count()}</div>
             <button onClick={() => setCount(count() + 1)}>Add One</button>
         </div>
